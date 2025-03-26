02ffda53fb9ee8bed560a583249a0811b087493c
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-03-26 22:26:05 +0900

    create project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:number_plate_app/game_play_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
