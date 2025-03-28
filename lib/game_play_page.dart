71af32b49e954517f4f966ab036044c3061d9491
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-03-28 20:17:23 +0900

    Feat: 계산 기능 완성

--- lib/game_play_page.dart
+++ lib/game_play_page.dart
@@ -1,59 +1,155 @@
+import 'package:expressions/expressions.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class GamePlayPage extends StatelessWidget {
-  GamePlayPage({super.key});
+class GamePlayPage extends StatefulWidget {
+  const GamePlayPage({super.key});
 
-  final List<String> arr = ['+','-','x','÷','1','2','3','!','4','5','6','()'];
+  @override
+  State<GamePlayPage> createState() => _GamePlayPageState();
+}
+
+class _GamePlayPageState extends State<GamePlayPage> {
+  List<String> arr = [
+    '+',
+    '-',
+    '*',
+    '÷',
+    '1',
+    '2',
+    '3',
+    '^',
+    '4',
+    '5',
+    '6',
+    '()',
+  ];
+  Set<String> usedNumbers = {};
+
+  int targetNumber = Random().nextInt(900) + 100;
+  List<String> expression = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('NumberPlate'),
-      ),
+      appBar: AppBar(title: Text('NumberPlate')),
       body: Column(
         children: [
-          Expanded(
-            child: Container(
-            
-            ),
+          numberPlate(),
+          expressionBox(expression),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 150,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      usedNumbers.clear();
+                      expression.clear();
+                    });
+                  },
+                  child: Text('RESET',
+                    style: TextStyle(
+                      fontSize: 20
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(width: 30,),
+              Container(
+                width: 150,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    String expressionString = expression.join('');
+                    Expression exp = Expression.parse(expressionString);
+                    final evaluator = const ExpressionEvaluator();
+                    var result = evaluator.eval(exp, {});
+                    if(result == targetNumber){
+                      print('정답');
+                    }
+                  },
+                  child: Text('CHECK',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          Container(
-            padding: EdgeInsets.all(40),
-            height: 350,
-            child: keyPads(),
-          ),
+          SizedBox(height: 10,),
+          keyPads(),
         ],
-      )
+      ),
+    );
+  }
+
+  Widget numberPlate() {
+    return Container(
+      margin: EdgeInsets.all(30),
+      padding: EdgeInsets.all(30),
+      alignment: Alignment.center,
+      width: double.infinity,
+      child: Text(
+        '$targetNumber',
+        style: TextStyle(fontSize: 150, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget expressionBox(List<String> expression) {
+    return Expanded(
+      child: Container(
+        alignment: Alignment.center,
+        child: Text('${expression.join('')}', style: TextStyle(fontSize: 30)),
+      ),
     );
   }
 
   Widget keyPads() {
-    return GridView.count(
-      mainAxisSpacing: 5,
-      crossAxisSpacing: 10,
-      crossAxisCount: 4,
-      children: List.generate(
-        arr.length,
-        (i) => ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.amber,
-            foregroundColor: Colors.black,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20)
+    return Container(
+      height: 350,
+      padding: EdgeInsets.fromLTRB(40, 0, 40, 40),
+      child: GridView.count(
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 10,
+        crossAxisCount: 4,
+        children: List.generate(
+          arr.length,
+          (i) => ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.teal[400],
+              foregroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
             ),
-          ),
-          onPressed: () {},
-          child: Text(
-            arr[i],
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+            onPressed: () {
+              setState(() {
+                if (!usedNumbers.contains(arr[i])) {
+                  expression.add(arr[i]);
+                  usedNumbers.add(arr[i]);
+                }
+              });
+            },
+            child: Text(
+              arr[i],
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
           ),
         ),
       ),
     );
   }
-
 }
